4abb8abb723146a86e47caff7efbc46f6a3db2a5
Mao <maoacr@gmail.com>
2025-11-10 21:45:13 -0500

    Prueba de streamig y compatibilidad de browsers para Tropicana Bucaramanga

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { useRef, useEffect } from "react";
+//import { useRef, useEffect } from "react";
 import "./App.css";
 
-import Hls from "hls.js";
+// import Hls from "hls.js";
 
 function App() {
-  const audioRef = useRef(null);
+  // const audioRef = useRef(null);
 
-  useEffect(() => {
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource("http://master.letio.com/io/?gs=8574&dest=decibeles1");
-      hls.attachMedia(audioRef.current);
-    } else if (audioRef.current.canPlayType("application/vnd.apple.mpegurl")) {
-      audioRef.current.src =
-        "http://master.letio.com/io/?gs=8574&dest=decibeles1";
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (Hls.isSupported()) {
+  //     const hls = new Hls();
+  //     hls.loadSource("http://master.letio.com/io/?gs=8574&dest=decibeles1");
+  //     hls.attachMedia(audioRef.current);
+  //   } else if (audioRef.current.canPlayType("application/vnd.apple.mpegurl")) {
+  //     audioRef.current.src =
+  //       "http://master.letio.com/io/?gs=8574&dest=decibeles1";
+  //   }
+  // }, []);
+
+  const signal = "https://26683.live.streamtheworld.com/TR_BUCARAMANGAAAC.aac"
 
   return (
     <>
-      <audio ref={audioRef} controls />
+      <audio preload controls autoPlay>
+        <source src={signal} type="audio/aac" />
+      </audio> 
     </>
   );
 }
